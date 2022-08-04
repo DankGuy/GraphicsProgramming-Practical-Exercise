6e2df60ac37e3c10d5fd7f17118fa0cbabf10b5e
DankGuy <68477088+DankGuy@users.noreply.github.com>
2022-08-04 23:58:26 +0800

    P2Q2,3,4

--- Practical 2/1.cpp
+++ Practical 2/1.cpp
@@ -1,16 +1,25 @@
 #include <Windows.h>
+#include <iostream>
 #include <gl/GL.h>
 #include <string>
+#include <math.h>
+#include <GL\freeglut.h>
 
 // #pragma comment (lib, "OpenGL32.lib")
 
 #define WINDOW_TITLE "Practical 2 Exercise"
 void changeColor();
 void reset();
+float rgbDivide(int rgb);
+void starColorChanging();
+void rotateAngleFunc();
 
-int quesNo = 1;
-float tx = 0, ty = 0, tSpeed = 0.1;
+int quesNo = 4;
+double tx = 0, ty = 0, tSpeed = 0.1;
 std::string color;
+float red = rgbDivide(255), green = rgbDivide(255), blue = rgbDivide(0);
+int rotateAngle = 0.0;
+double scaleRatio = 0.0;
 
 
 // window procedure acts as something like event listener (listen for message or input), full of switch cases
@@ -42,6 +51,16 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 			color = "blue";
 		else if (wParam == 0x20)
 			reset();
+		else if (wParam == 0x31)
+			quesNo = 1;
+		else if (wParam == 0x32)
+			quesNo = 2;
+			//starColorChanging(0);
+		else if (wParam == 0x33)
+			quesNo = 3;
+			//rotateAngleTimer(0);
+		else if (wParam == 0x34)
+			quesNo = 4;
 		break;
 		
 	default:
@@ -85,6 +104,11 @@ bool initPixelFormat(HDC hdc)
 }
 //--------------------------------------------------------------------
 
+// divide 255
+float rgbDivide(int rgb) {
+	return ((float)rgb) / 255;
+}
+
 // Q1
 void q1() {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -116,7 +140,140 @@ void reset() {
 }
 
 // Q2
+void drawTriangle(float rotate) {
+	glPushMatrix();
+	glRotatef(rotate, 0, 0, 1);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(0, 0.6);
+		glVertex2f(-0.178, 0.24);
+		glVertex2f(0.178, 0.24);
+	glEnd();
+	glPopMatrix();
+}
+void starColorChanging() {
+	//glutPostRedisplay();
+	//glutTimerFunc(1000 / 60, starColorChanging, 0);
+	if (red == 1.0) {
+		red = 0.0;
+		blue = 0.0;
+	}
+	else if (green == 1.0) {
+		green = 0.0;
+		blue = 1.0;
+	}
+	else if (blue == 1.0) {
+		blue = 0.0;
+		red = 1.0;
+	}
+	else {
+		red = 1.0;
+		green = 1.0;
+		blue = 0.0;
+	}
+}
+void q2() {
+	float PI = 3.1429;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(red, green, blue); // yellow
+	starColorChanging();
 
+
+	// hexagon
+	glPushMatrix();
+	glRotatef(54, 0, 0, 1);
+	glBegin(GL_TRIANGLE_FAN);
+	for (float angle = 0; angle <= (2 * PI); angle += (2 * PI) / 5) {
+		float x = 0 + 0.3 * cos(angle);
+		float y = 0 + 0.3 * sin(angle);
+		glVertex2f(x, y);
+	}
+	glEnd();
+	glPopMatrix();
+
+	// triangles
+	drawTriangle(0.0);
+	drawTriangle(73.0);
+	drawTriangle(-73.0);
+	drawTriangle(145.0);
+	drawTriangle(-145.0);
+
+	// lines
+	glPushMatrix();
+	glTranslatef(0, -0.05, 0);
+	glLineWidth(5);
+	glBegin(GL_LINES);
+	glColor3f(rgbDivide(105), rgbDivide(105), rgbDivide(105)); // dimgray
+		glVertex2f(0, 0.3);
+		glVertex2f(0, 0.1);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2f(-0.15, 0.135);
+		glVertex2f(0, 0.1);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2f(0.15, 0.135);
+		glVertex2f(0, 0.1);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2f(0, 0.1);
+		glVertex2f(-0.1, -0.1);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2f(0, 0.1);
+		glVertex2f(0.1, -0.1);
+	glEnd();
+	glPopMatrix();
+}
+
+// Q3
+void rotateAngleFunc() {
+	//glutPostRedisplay();
+	//glutTimerFunc(1000 / 60, rotateAngleTimer, 0);
+	
+	if (rotateAngle < 360) {
+		rotateAngle += 1.0;
+	}
+	else {
+		rotateAngle = 0.0;
+	}
+}
+void q3() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	rotateAngleFunc();
+	glLoadIdentity();
+	glRotatef(rotateAngle, 0, 0, 1);
+	glPointSize(10);
+	glBegin(GL_POINTS);
+		glColor3f(1, 1, 1);
+		glVertex2f(0, -0.5);
+	glEnd();
+}
+
+// Q4
+void expand() {
+	if (scaleRatio < 9.5) {
+		scaleRatio += 0.1;
+	}
+}
+void q4() {
+	glLoadIdentity();
+	glScalef(scaleRatio, scaleRatio, 0.0);
+	expand();
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0, 0);
+	glBegin(GL_QUADS);
+		glVertex2f(-0.1, -0.1);
+		glVertex2f(0.1, -0.1);
+		glVertex2f(0.1, 0.1);
+		glVertex2f(-0.1, 0.1);
+	glEnd();
+}
 
 void display()
 {
@@ -154,6 +311,15 @@ void display()
 	case 1:
 		q1();
 		break;
+	case 2:
+		q2();
+		break;
+	case 3:
+		q3();
+		break;
+	case 4:
+		q4();
+		break;
 	default:
 		q1();
 		break;
@@ -161,6 +327,7 @@ void display()
 
 }
 //--------------------------------------------------------------------
+
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 {
